5f43c4d34975024c1a6855a1b3411f7816be7532
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-08 16:12:58 +0530

    Implementing the Winner Logic
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- jsgame.js
+++ jsgame.js
@@ -10,6 +10,7 @@ function printx(number){
         isko.innerText=sign;
         checksign()
         disp.innerHTML = "<center>"+ sign + " ka turn hai"+"</center>" 
+        winner();
     }
     
 }
@@ -21,4 +22,26 @@ function checksign(){
     else{
         sign='x'
     }
+}
+
+function getBox(no){
+    return document.getElementById("r"+no).innerHTML;
+}
+
+function checkMove(a,b,c,m){
+    if(getBox(a)==m && getBox(b)==m && getBox(c)==m){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function winner(){
+    if(checkMove(1,2,3,"x") || checkMove(4,5,6,"x") || checkMove(7,8,9,"x") ||
+        checkMove(1,4,7,"x") || checkMove(2,5,8,"x") || checkMove(3,6,9,"x") ||
+              checkMove(1,5,9,"x") || checkMove(7,5,3,"x") ){
+                 
+                    alert("X is Winner");
+            }         
 }
